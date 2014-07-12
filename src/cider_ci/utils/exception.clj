9d50c4f24526225cfe6fd674655aed2471170238
Thomas Schank <DrTom@schank.ch>
2014-07-12 14:00:46 +0200

    Fix exception stringification

--- src/cider_ci/utils/exception.clj
+++ src/cider_ci/utils/exception.clj
@@ -5,8 +5,16 @@
 
 (ns cider-ci.utils.exception
   (:require 
+    [clj-logging-config.log4j :as logging-config]
     [clojure.stacktrace :as stacktrace]
+    [clojure.tools.logging :as logging]
+    [clojure.string :as string]
     ))
+
+;(logging-config/set-logger! :level :debug)
+;(logging-config/set-logger! :level :info)
+
+
 
 
 (defn expand-to-seq [^Throwable tr]
@@ -18,8 +26,7 @@
 
 (defn trace-string-seq [ex]
   (map (fn [e] (with-out-str (stacktrace/print-trace-element e)))
-       (.getStackTrace ex)
-       ))
+       (.getStackTrace ex)))
 
 ;(trace-string-seq (IllegalStateException. "asdfa"))
 
@@ -33,11 +40,12 @@
 ;(filter-trace-string-seq (trace-string-seq (IllegalStateException. "asdfa")))
 
 (defn stringify [^Throwable tr]
-  (map 
-    (fn [ex]
-      (str [ (with-out-str (stacktrace/print-throwable ex))
-            (filter-trace-string-seq (trace-string-seq ex))]))
-    (expand-to-seq tr)))
-
+  (string/join ", " (map 
+                      (fn [ex]
+                        (logging/debug {:ex ex})
+                        (str [ (with-out-str (stacktrace/print-throwable ex))
+                              (filter-trace-string-seq (trace-string-seq ex))]))
+                      (expand-to-seq tr))))
+  
 ;(stringify (IllegalStateException. "A"))
 ;(stringify (java.sql.SQLException. "SQ"))
