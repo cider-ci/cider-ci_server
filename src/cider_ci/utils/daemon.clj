954a3cdd34283330397fb9531bf29bbea5e3b611
Thomas Schank <DrTom@schank.ch>
2014-07-17 21:06:58 +0200

    Add (multiple) daemon(s) to the ns via the daemon/define macro

---
+++ src/cider_ci/utils/daemon.clj
@@ -0,0 +1,42 @@
+; Copyright (C) 2013, 2014 Dr. Thomas Schank  (DrTom@schank.ch, Thomas.Schank@algocon.ch)
+; Licensed under the terms of the GNU Affero General Public License v3.
+; See the "LICENSE.txt" file provided with this software.
+
+(ns cider-ci.utils.daemon
+  (:require 
+    [clj-logging-config.log4j :as logging-config]
+    [clojure.tools.logging :as logging]
+    [cider-ci.utils.with :as with]
+    ))
+
+
+(defmacro define [daemon-name start-fn stop-fn secs-pause & body]
+  (let [stop (gensym "_stop_")]
+    `(do 
+
+       (defonce ~stop (atom (fn [])))
+
+       (defn ~stop-fn []
+         (@~stop))
+
+       (defn ~start-fn []
+         (~stop-fn)
+         (let [done# (atom false)
+               runner# (future (logging/debug "daemon " ~daemon-name " started")
+                               ;(Thread/sleep (* ~secs-pause 1000))
+                               (loop []
+                                 (when-not @done#
+                                   (with/suppress-and-log-error
+                                     ~@body)
+                                   (Thread/sleep (* ~secs-pause 1000))
+                                   (recur))))]
+           (reset! ~stop (fn []
+                           (reset! done# true)
+                           (future-cancel runner#)
+                           ;@runner#
+                           (logging/debug "daemon " ~daemon-name "stopped")
+                           ))))
+       )))
+
+;(macroexpand-1 '(define "Blah" start stop 10 (logging/info "looping ...")))
+
