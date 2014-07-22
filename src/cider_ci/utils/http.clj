383cc6c3afbf360780433933a4052b0b41acc23d
Thomas Schank <DrTom@schank.ch>
2014-07-22 15:23:35 +0200

    Add http/put and http/authenticate

--- src/cider_ci/utils/http.clj
+++ src/cider_ci/utils/http.clj
@@ -1,9 +1,10 @@
 (ns cider-ci.utils.http
   (:require
+    [cider-ci.utils.with :as with]
     [clj-http.client :as http-client]
     [clj-logging-config.log4j :as logging-config]
     [clojure.tools.logging :as logging]
-    [cider-ci.utils.with :as with]
+    [ring.middleware.basic-authentication :refer [wrap-basic-authentication]]
     ))
 
 ;(logging-config/set-logger! :level :debug)
@@ -27,7 +28,39 @@
                :conn-timeout 1000 }
               params)))))
 
+; TODO dry-up
+(defn put [url params]
+  (logging/debug post [url params])
+  (let [basic-auth (:basic_auth @conf)]
+    (with/logging
+      (logging/debug "http/put" {:url url :basic-auth basic-auth})
+      (http-client/put
+        url
+        (conj {:basic-auth [(:user basic-auth) (:secret basic-auth)]
+               :insecure? true
+               :content-type :json
+               :accept :json 
+               :socket-timeout 1000  
+               :conn-timeout 1000 }
+              params)))))
+
+
+
 (defn initialize [new-conf]
   (reset! conf new-conf))
+
+
+;### Http Basic Authentication ################################################
+
+(defn authenticated? [application password]
+  (logging/debug authenticated? [application password])
+  (and 
+    (= password
+       (-> @conf (:basic_auth) (:secret)))
+    (keyword application)))
+
+
+(defn authenticate [handler]
+   (wrap-basic-authentication handler authenticated?))
 
 
