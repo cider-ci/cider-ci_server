dfb2d5a16b8366605f35b079a07f74e89f62e346
Thomas Schank <DrTom@schank.ch>
2014-08-10 18:42:25 +0200

    Extend protocol IResultSetReadColumn to handle pg arrays

--- src/cider_ci/utils/rdbms/json.clj
+++ src/cider_ci/utils/rdbms/json.clj
@@ -3,8 +3,12 @@
 ; See the "LICENSE.txt" file provided with this software.
 
 (ns cider-ci.utils.rdbms.json
-  (:require [clojure.java.jdbc :as jdbc]
-            [clojure.data.json :as json])
+  (:require 
+    [clj-logging-config.log4j :as logging-config]
+    [clojure.data.json :as json]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.tools.logging :as logging]
+    )
   (:import org.postgresql.util.PGobject))
 
 (defn value-to-json-pgobject [value]
@@ -24,7 +28,21 @@
   (result-set-read-column [pgobj metadata idx]
     (let [type  (.getType pgobj)
           value (.getValue pgobj)]
+      ;(logging/debug [type value])
       (case type
         "json" (json/read-str value)
         :else value))))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  org.postgresql.jdbc4.Jdbc4Array 
+  (result-set-read-column [pgobj metadata idx]
+    (logging/debug [pgobj metadata idx])
+    (.getArray pgobj)))
+
+
+;### Debug ####################################################################
+;(debug/debug-ns *ns*)
+;(logging-config/set-logger! :level :debug)
+;(logging-config/set-logger! :level :info)
+
 
