6137ffd0f2dec2d5bfbf6837772c5f8269784b55
Thomas Schank <DrTom@schank.ch>
2014-08-22 21:31:16 +0200

    Set version number to 1.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider-ci/clj-utils "1.6.1"
+(defproject cider-ci/clj-utils "1.7.0"
   :description "Shared Clojure Utils for Cider-CI"
   :url "https://github.com/cider-ci/cider-ci_clj-utils"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
