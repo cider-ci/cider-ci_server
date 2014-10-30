a1031f14e06c159192441b9cfafc169434fd93d0
Thomas Schank <DrTom@schank.ch>
2014-10-30 13:58:27 +0100

    Adjust tests for new hierarchic task naming convention

--- test/cider_ci/builder/tasks_spec.clj
+++ test/cider_ci/builder/tasks_spec.clj
@@ -36,10 +36,11 @@
 
 (facts "build-task" 
        (let [built-task (build-task 
-                           test-task-definition 
-                           test-task-defaults
-                           test-script-defaults)]
-         (fact (:name built-task) => "Task")
+                          test-task-definition 
+                          test-task-defaults
+                          test-script-defaults
+                          "Prefix")]
+         (fact (:name built-task) => "Prefix » Task")
          (fact (:ports built-task) =>  {:web {:min 8800 :max 89999} :xvnc {:min 5900 :max 5999}})
          (fact (-> built-task :scripts :main :environment_variables) =>  {:x 1 :y 2 :z 3})
          ))
@@ -47,17 +48,17 @@
 (facts "build-tasks-for-contexts-sequence"
        (let [spec (yaml/parse-string 
                     (slurp "test/data/tasks_spec.yml"))
-             tasks (build-tasks-for-contexts-sequence spec {} {})
-             task_in_l1-2-3  (some #(when (= (:name %) "Task in L1.2.3")%) tasks)
+             tasks (build-tasks-for-contexts-sequence spec {} {} "Prefix")
+             task_in_l1-2-3  (some #(when (= (:name %) "Prefix » L1 » L1.2 » L1.2.3 » Task in L1.2.3")%) tasks)
              ]
          (fact tasks => truthy)
          (fact (count tasks) => 6)
-         (fact (->> tasks (map :name) sort) =>  (sort ["Task in L1" 
-                                                       "Task in L1.1" 
-                                                       "Task 1 in L1.2" 
-                                                       "Task 2 in L1.2" 
-                                                       "Task in L1.2.3"
-                                                       "Task in L2"]))
+         (fact (->> tasks (map :name) sort) =>  (sort ["Prefix » L1 » L1.1 » Task in L1.1" 
+                                                       "Prefix » L1 » L1.2 » L1.2.3 » Task in L1.2.3" 
+                                                       "Prefix » L1 » L1.2 » Task 1 in L1.2" 
+                                                       "Prefix » L1 » L1.2 » Task 2 in L1.2" 
+                                                       "Prefix » L1 » Task in L1" 
+                                                       "Prefix » L2 » Task in L2"]))
          (fact task_in_l1-2-3 => truthy)
          (fact (:scripts task_in_l1-2-3) => {:main {:environment_variables {:e 7}}})
          ))
