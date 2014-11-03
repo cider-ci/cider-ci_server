4c94e09dd81235a33b54b78d70d8846c333966e7
Thomas Schank <DrTom@schank.ch>
2014-11-03 22:17:59 +0100

    Upgrade clj-auth to fix basic-auth issue

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-auth "2.0.0"]
+                 [cider-ci/clj-auth "2.0.1"]
                  [cider-ci/clj-utils "2.0.0"]
                  [clj-jgit "0.8.0"]
                  [org.clojure/tools.nrepl "0.2.6"]
