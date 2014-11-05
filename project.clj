b170629f0348c28ad4d2257a2edb6aa364d4677c
Thomas Schank <DrTom@schank.ch>
2014-11-05 22:23:58 +0100

    Upgrade clj-auth to 2.0.1, fixes basic auth for user

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cider-ci_storage "2.0.0"
+(defproject cider-ci_storage "2.0.1"
   :description "Cider-CI Storage"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-auth "2.0.0"]
+                 [cider-ci/clj-auth "2.0.1"]
                  [cider-ci/clj-utils "2.0.0"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/tools.nrepl "0.2.6"]
