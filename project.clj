e9a79518558d492c7acf72de3b380588bfcbd846
Thomas Schank <DrTom@schank.ch>
2014-11-06 15:38:24 +0100

    Improve url building

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [cider-ci/clj-auth "2.0.1"]
-                 [cider-ci/clj-utils "2.0.0"]
+                 [cider-ci/clj-utils "2.0.1-rc.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.jruby/jruby-complete "1.7.13"]
                  [sqlingvo "0.6.5"]
