d355752804d24420b023fd97f16d6b509bbfe829
Thomas Schank <DrTom@schank.ch>
2015-01-07 16:20:51 +0100

    Upgrade dependencies; update copyright notices

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
-(defproject cider-ci_builder "1.1.0-rc.1"
+; Copyright (C) 2013, 2014, 2015 Dr. Thomas Schank  (DrTom@schank.ch, Thomas.Schank@algocon.ch)
+; Licensed under the terms of the GNU Affero General Public License v3.
+; See the "LICENSE.txt" file provided with this software.
+
+(defproject cider-ci_builder "1.1.0"
   :description "Cider-CI Builder"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [cider-ci/clj-auth "2.0.0"]
-                 [cider-ci/clj-utils "2.3.0-rc.2"]
+                 [cider-ci/clj-utils "2.3.0"]
                  [drtom/clj-uuid "0.0.8"]
                  [org.clojure/tools.nrepl "0.2.6"]
                  ]
