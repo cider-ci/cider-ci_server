f7d801d6e7de6c0962a80e164c8ecc55f7559d6f
Thomas Schank <DrTom@schank.ch>
2015-01-22 10:27:15 +0100

    Fix yaml parse error through upgrade dependencies, set new development defaults

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
-(defproject cider-ci_builder "1.1.0"
+(defproject cider-ci_builder "1.1.1"
   :description "Cider-CI Builder"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-auth "2.0.0"]
-                 [cider-ci/clj-utils "2.3.0"]
+                 [cider-ci/clj-auth "2.1.0"]
+                 [cider-ci/clj-utils "2.4.0"]
                  [drtom/clj-uuid "0.0.8"]
-                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.nrepl "0.2.7"]
                  ]
   :source-paths [ "src"]
   :profiles {
@@ -21,4 +21,5 @@
   :aot [cider-ci.builder.main] 
   :main cider-ci.builder.main 
   :repositories [["tmp" {:url "http://maven-repo-tmp.drtom.ch" :snapshots false}]]
+  :jvm-opts ["-Xmx128m"]
   )
