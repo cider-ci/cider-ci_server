f9ad33a82f7d1f5e5384724b4f401a28e9981590
Thomas Schank <DrTom@schank.ch>
2015-01-23 19:13:17 +0100

    Set new development defaults; upgrade dependencies

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [cider-ci/clj-auth "2.1.0"]
-                 [cider-ci/clj-utils "2.3.0"]
+                 [cider-ci/clj-utils "2.4.0"]
                  [honeysql "0.4.3"]
-                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.nrepl "0.2.7"]
                  ]
   :source-paths ["src"]
   :profiles {
@@ -20,4 +20,5 @@
   :aot [cider-ci.dispatcher.main] 
   :main cider-ci.dispatcher.main 
   :repositories [["tmp" {:url "http://maven-repo-tmp.drtom.ch" :snapshots false}]]
+  :jvm-opts ["-Xmx256m"]
   )
