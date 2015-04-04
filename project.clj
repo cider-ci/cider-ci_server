05554187b6dad73c5d67f2b4a117ba0d3654acc0
Thomas Schank <DrTom@schank.ch>
2015-04-04 21:54:36 +0200

    Use cider-ci open-session

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-auth "2.2.2"]
-                 [cider-ci/clj-utils "2.13.0-beta.2"]
+                 [cider-ci/clj-auth "3.0.0-beta.4"]
+                 [cider-ci/clj-utils "3.0.0-beta.1"]
                  [clj-jgit "0.8.3"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/tools.nrepl "0.2.8"]
