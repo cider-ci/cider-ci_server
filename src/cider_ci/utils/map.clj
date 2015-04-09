9d7aa5f550c825ffc9b4d311af322ce34f0b9467
Thomas Schank <DrTom@schank.ch>
2015-04-09 20:55:52 +0200

    3.0.0-beta.4 add map/convert-to-array

--- src/cider_ci/utils/map.clj
+++ src/cider_ci/utils/map.clj
@@ -14,3 +14,24 @@
     (apply merge-with deep-merge vals)
     (last vals)))
 
+
+(defn convert-to-array 
+  "Converts a map of maps to an array of maps.
+  The key of will become the value of the :name 
+  property if it did no exists already."
+  [map-or-array]
+  (if (and (map? map-or-array)
+           (every?  map? (map second map-or-array)))
+    (map (fn [[k m]]
+           (if (:name m) 
+             m
+             (assoc m :name k)))
+         map-or-array)
+    map-or-array))
+
+
+;(convert-to-array {:x {:y 1}})
+;(convert-to-array {:x {:name 1}})
+;(convert-to-array [{:name "x"}])
+
+
