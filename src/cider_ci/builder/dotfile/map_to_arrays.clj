e7d9c192ed27ca0ab52200aed71837112fa6b6f6
Thomas Schank <DrTom@schank.ch>
2015-05-15 16:03:14 +0200

    Extract map-to-array out of dotfile

--- src/cider_ci/builder/dotfile/map_to_arrays.clj
+++ src/cider_ci/builder/dotfile/map_to_arrays.clj
@@ -2,32 +2,27 @@
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
-(ns cider-ci.builder.dotfile
+(ns cider-ci.builder.dotfile.map-to-arrays
   (:require 
-    [cider-ci.builder.dotfile.inclusion :as inclusion]
-    [cider-ci.builder.dotfile.map-to-arrays :as map-to-arrays]
-
-    [cider-ci.builder.repository :as repository]
     [cider-ci.utils.map :as map]
     [clj-logging-config.log4j :as logging-config]
-    [clojure.core.memoize :as memo]
     [clojure.tools.logging :as logging]
     [drtom.logbug.debug :as debug]
     ))
 
+(declare map-to-arrays)
 
-(defn- get-dotfile_ [tree-id]
-  (->> 
-    (repository/get-path-content tree-id ".cider-ci.yml")
-    (inclusion/include tree-id)
-    map-to-arrays/map-to-arrays
-    ))
+(defn map-key-val-to-array [k spec]
+  (if-let [moa (spec k)]
+    (assoc spec k (map map-to-arrays (map/convert-to-array moa)))
+    spec)) 
 
-(def get-dotfile (memo/lru #(get-dotfile_ %)
-            :lru/threshold 500))
 
-; TODO re-enable caching when done
-(def get-dotfile get-dotfile_)
+(defn map-to-arrays [spec]
+  (->> spec
+       (map-key-val-to-array :jobs)
+       (map-key-val-to-array :tasks)
+       (map-key-val-to-array :subcontexts)))
 
 ;### Debug ####################################################################
 ;(logging-config/set-logger! :level :debug)
