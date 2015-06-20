e074e2326487612b163edfa34ff39f81e08173d4
Thomas Schank <DrTom@schank.ch>
2015-06-20 09:31:01 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
-(defproject cider-ci_api "3.0.0-rc.1"
+(defproject cider-ci_api "3.0.0"
   :description "Cider-CI API"
   :url "https://github.com/cider-ci/cider-ci_api"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cheshire "5.4.0"]
-                 [cider-ci/clj-auth "3.0.0-rc.1"]
-                 [cider-ci/clj-utils "3.0.0-rc.4"]
+                 [cheshire "5.5.0"]
+                 [cider-ci/clj-auth "3.0.0"]
+                 [cider-ci/clj-utils "3.0.0"]
                  [drtom/honeysql "1.1.0"]
                  [json-roa/clj-utils "1.0.0"]
                  [org.clojure/tools.nrepl "0.2.10"]
