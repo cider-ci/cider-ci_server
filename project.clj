1f63cc82f2ddf0fdc69007f1e26f1fcf0e05c0ef
Thomas Schank <DrTom@schank.ch>
2015-06-20 09:24:31 +0200

    Set version to 3.0.0; upgrade deps; remove tests

--- project.clj
+++ project.clj
@@ -2,27 +2,27 @@
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
-(defproject cider-ci/clj-utils "3.0.0-rc.4"
+(defproject cider-ci/clj-utils "3.0.0"
   :description "Shared Clojure Utils for Cider-CI"
   :url "https://github.com/cider-ci/cider-ci_clj-utils"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cheshire "5.4.0"]
-                 [clj-http "1.1.1"]
+                 [cheshire "5.5.0"]
+                 [clj-http "1.1.2"]
                  [clj-time "0.9.0"]
                  [clj-yaml "0.4.0"]
                  [com.mchange/c3p0 "0.9.5"]
                  [com.novemberain/langohr "3.2.0"]
-                 [compojure "1.3.3"]
+                 [compojure "1.3.4"]
                  [drtom/clj-uuid "0.0.8"]
-                 [drtom/logbug "1.0.0"]
-                 [joda-time "2.7"]
+                 [drtom/logbug "1.1.0"]
+                 [joda-time "2.8.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [org.clojars.hozumi/clj-commons-exec "1.2.0"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.clojure/java.jdbc "0.3.7"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]
@@ -39,7 +39,7 @@
 
   :profiles {:dev {:dependencies [
                                   [midje "1.6.3"]
-                                  [org.xerial/sqlite-jdbc "3.8.7"]
+                                  [org.xerial/sqlite-jdbc "3.8.10.1"]
                                   ]
                    :plugins [[lein-midje "3.1.1"]]
                    :resource-paths ["resources_dev"]
