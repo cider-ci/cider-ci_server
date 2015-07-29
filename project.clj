c0b0000ff1f0604b6895b6b4f503dcb346ef9201
Thomas Schank <DrTom@schank.ch>
2015-07-29 22:32:37 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [cider-ci/clj-auth "3.0.0"]
-                 [cider-ci/clj-utils "3.0.1"]
+                 [cider-ci/clj-utils "3.0.3"]
                  [honeysql "0.6.1"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/core.memoize "0.5.7"]
