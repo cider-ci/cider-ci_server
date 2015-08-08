b67dec3f9bc8922bc5942957a17ec7cea21064c3
Thomas Schank <DrTom@schank.ch>
2015-08-08 22:55:04 +0200

    Detect and fail lost trials on executor

--- src/cider_ci/dispatcher/sync.clj
+++ src/cider_ci/dispatcher/sync.clj
@@ -9,15 +9,41 @@
     [cider-ci.dispatcher.sync.ping :as ping]
     [cider-ci.dispatcher.sync.sync-trials :as sync-trials]
     [cider-ci.dispatcher.sync.update-executor :as update-executor]
+    [cider-ci.dispatcher.trial :as trial]
+    [cider-ci.utils.rdbms :as rdbms]
     [clj-logging-config.log4j :as logging-config]
+    [clojure.data.json :as json]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.set :refer [difference]]
     [clojure.tools.logging :as logging]
     [drtom.logbug.catcher :as catcher]
-    [clojure.data.json :as json]
     ))
+
+
+(defn in-progress-trials-of-executor [executor]
+  "Trials believed being in progress on executor
+   and which have not been updated within the last minute."
+  (jdbc/query (rdbms/get-ds)
+              ["SELECT trials.id, state, started_at, finished_at FROM trials
+               JOIN executors ON trials.executor_id = executors.id
+               WHERE executors.id = ?
+               AND state IN ('dispatching', 'executing', 'aborting')
+               AND (trials.updated_at < (now() - interval '1 Minutes'))
+               " (:id executor)]))
+
+(defn fail-trials-lost-on-executor [executor reported-executor-trials]
+  (let [known-trial-ids-by-executor (map :trial_id reported-executor-trials)
+        in-progress-trials-of-executor (in-progress-trials-of-executor executor)
+        in-progress-trials-of-executor-ids (map #(-> % :id str) in-progress-trials-of-executor)]
+    (doseq [lost-trial-id  (difference (set in-progress-trials-of-executor-ids)
+                                       (set known-trial-ids-by-executor))]
+      (trial/update {:id lost-trial-id
+                     :state "aborted"
+                     :error (str "This trial was lost on executor " (:name executor))}))))
 
 (defn sync [executor data]
   (catcher/wrap-with-log-warn
-    (logging/debug 'sync executor data)
+    (fail-trials-lost-on-executor executor (:trials data))
     (update-executor/update-when-changed executor data)
     (ping/update-last-ping-at executor)
     ;jobs-to-execute (get-jobs-to-execute executor (:body request))]
