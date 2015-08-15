34962ec4bdb36debc14102e15a159555be1d6d66
Thomas Schank <DrTom@schank.ch>
2015-08-15 14:56:08 +0200

    3.2.0-beta.1 Add honeysql shortcuts and add defdaemon

--- src/cider_ci/utils/daemon.clj
+++ src/cider_ci/utils/daemon.clj
@@ -10,7 +10,46 @@
     ))
 
 
-(defmacro define [daemon-name start-fn stop-fn secs-pause & body]
+
+(defmacro defdaemon [daemon-name secs-pause & body]
+  (let [stop (gensym "_stop_")
+        start-fn (symbol (str "start-" daemon-name))
+        stop-fn (symbol (str "stop-" daemon-name))]
+    `(do
+
+       (defonce ~stop (atom (fn [])))
+
+       (defn ~stop-fn []
+         (@~stop))
+
+       (defn ~start-fn []
+         (~stop-fn)
+         (let [done# (atom false)
+               runner# (future (logging/info "daemon " ~daemon-name " started")
+                               (loop []
+                                 (when-not @done#
+                                   (catcher/wrap-with-suppress-and-log-error
+                                     ~@body)
+                                   (Thread/sleep (Math/ceil (* ~secs-pause 1000)))
+                                   (recur))))]
+           (reset! ~stop (fn []
+                           (reset! done# true)
+                           (future-cancel runner#)
+                           ;@runner#
+                           (logging/info "daemon " ~daemon-name "stopped")))
+
+           (.addShutdownHook (Runtime/getRuntime)
+                             (Thread. (fn [] (~stop-fn))))
+
+           )))))
+
+;(macroexpand-1 '(defdeamon "blah" 10 (logging/info "looping ...")))
+
+
+(defmacro define
+  {:deprecated "3.2"}
+  [daemon-name start-fn stop-fn secs-pause & body]
+  (logging/warn "daemon/define is deprecated use defdaemon")
   (let [stop (gensym "_stop_")]
     `(do
 
@@ -41,5 +80,5 @@
            )))))
 
 
-;(macroexpand-1 '(define "Blah" start stop 10 (logging/info "looping ...")))
+;(macroexpand-1 '(define "blah"  10 (logging/info "looping ...")))
 
