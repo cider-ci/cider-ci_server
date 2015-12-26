7d679084b76febf97378161b95f73794bc04e7c2
Thomas Schank <DrTom@schank.ch>
2015-12-26 21:18:36 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
-                 [cider-ci/clj-auth "4.0.0"]
-                 [cider-ci/clj-utils "3.3.2"]
+                 [cider-ci/clj-auth "4.0.1"]
+                 [cider-ci/clj-utils "4.0.7"]
                  [drtom/honeysql "1.3.0-beta.4"]
                  [logbug "2.0.0-beta.8"]
                  [clojurewerkz/urly "1.0.0"]
--- src/cider_ci/dispatcher/sync/update_executor.clj
+++ src/cider_ci/dispatcher/sync/update_executor.clj
@@ -5,14 +5,18 @@
 (ns cider-ci.dispatcher.sync.update-executor
   (:require
     [cider-ci.dispatcher.executor :as executor-entity]
-    [logbug.debug :as debug]
     [cider-ci.utils.http :as http]
     [cider-ci.utils.rdbms :as rdbms]
-    [logbug.catcher :as catcher]
-    [clj-logging-config.log4j :as logging-config]
     [clojure.data.json :as json]
     [clojure.java.jdbc :as jdbc]
+
+
+    [clj-logging-config.log4j :as logging-config]
     [clojure.tools.logging :as logging]
+    [logbug.catcher :as catcher :refer [catch*]]
+    [logbug.debug :as debug]
+    [logbug.ring :refer [wrap-handler-with-logging]]
+
     ))
 
 
@@ -42,4 +46,8 @@
 
   )
 
-
+;#### debug ###################################################################
+;(debug/debug-ns *ns*)
+;(debug/re-apply-last-argument #'update-when-changed)
+;(logging-config/set-logger! :level :debug)
+;(logging-config/set-logger! :level :info)
