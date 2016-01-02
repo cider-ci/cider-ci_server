0d529a130fca0ba34c2bed8dc3ca2a72e79085ba
Thomas Schank <DrTom@schank.ch>
2016-01-02 09:17:48 +0100

    Upgrade to clj-utils 6.0.1

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [cider-ci/clj-auth "5.0.1"]
-                 [cider-ci/clj-utils "6.0.0"]
+                 [cider-ci/clj-utils "6.0.1"]
 
                  [drtom/honeysql "1.3.0-beta.4"]
                  [logbug "3.0.0"]
