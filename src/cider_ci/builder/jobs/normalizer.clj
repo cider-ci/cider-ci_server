d14bac40f96492266221d03d82e57189d6511ca5
Thomas Schank <DrTom@schank.ch>
2016-01-19 21:15:05 +0100

    Fix compact notation normalization
    ... by upgrading clj-utils resp clj-types

--- src/cider_ci/builder/jobs/normalizer.clj
+++ src/cider_ci/builder/jobs/normalizer.clj
@@ -168,4 +168,4 @@
 ;### Debug ####################################################################
 ;(logging-config/set-logger! :level :debug)
 ;(logging-config/set-logger! :level :info)
-(debug/debug-ns *ns*)
+;(debug/debug-ns *ns*)
