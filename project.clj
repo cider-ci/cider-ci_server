240c508ad2d768f7fdeaac139205e343d5a512a9
Thomas Schank <DrTom@schank.ch>
2016-02-05 20:03:46 +0100

    7.0.0 upgrade dependencies
    * and to logbug 4 in particular which has breaking changes

--- project.clj
+++ project.clj
@@ -2,26 +2,26 @@
 ; Licensed under the terms of the GNU Affero General Public License v3.
 ; See the "LICENSE.txt" file provided with this software.
 
-(defproject cider-ci/clj-utils "6.1.1"
+(defproject cider-ci/clj-utils "7.0.0"
   :description "Shared Clojure Utils for Cider-CI"
   :url "https://github.com/cider-ci/cider-ci_clj-utils"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [
                  [drtom/honeysql "1.3.0-beta.4"]
-                 [logbug "3.0.0"]
+                 [logbug "4.0.0"]
                  [pg-types "2.1.2"]
 
                  [cheshire "5.5.0"]
-                 [clj-http "2.0.0"]
+                 [clj-http "2.0.1"]
                  [clj-time "0.11.0"]
                  [clj-yaml "0.4.0"]
                  [com.github.mfornos/humanize-slim "1.2.2"]
                  [com.mchange/c3p0 "0.9.5"] ; Don not upgrade this. It depends on Java 1.8.
-                 [com.novemberain/langohr "3.4.2"]
+                 [com.novemberain/langohr "3.5.1"]
                  [compojure "1.4.0"]
                  [danlentz/clj-uuid "0.1.6"]
-                 [joda-time "2.9.1"]
+                 [joda-time "2.9.2"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [me.raynes/fs "1.4.6"]
                  [org.apache.commons/commons-lang3 "3.4"]
@@ -30,7 +30,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [org.clojure/tools.nrepl "0.2.12"]
-                 [org.slf4j/slf4j-log4j12 "1.7.13"]
+                 [org.slf4j/slf4j-log4j12 "1.7.14"]
                  [org.yaml/snakeyaml "1.16"]
                  [ring "1.4.0"]
                  [ring/ring-core "1.4.0"]
