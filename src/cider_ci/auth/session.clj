38a253eff5a73fd43e10f5780c76359303ba3ec6
Thomas Schank <DrTom@schank.ch>
2016-02-27 15:51:32 +0100

    8.2.0 Validate session expiration

--- src/cider_ci/auth/session.clj
+++ src/cider_ci/auth/session.clj
@@ -5,6 +5,9 @@
 
 (ns cider-ci.auth.session
   (:require
+    [clj-time.core :as time]
+    [cider-ci.utils.duration :as duration]
+    [clj-time.format :as time-format]
     [logbug.debug :as debug]
     [cider-ci.utils.rdbms :as rdbms]
     [logbug.catcher :as catcher]
@@ -28,7 +31,23 @@
 (defn get-session-secret []
   (-> (get-config) :session :secret))
 
-; TODO check session expiration
+(defn validate-session-lifetime! [valid-for-secs issued-at]
+  (let [expires-at (time/plus issued-at (time/seconds valid-for-secs))]
+    (when (time/before? expires-at (time/now))
+      (throw (IllegalStateException. "Session has expired")))))
+
+(defn validate-expiration! [user session-object]
+  (let [issued-at-str (-> session-object :issued_at)]
+    (when (clojure.string/blank? issued-at-str)
+      (throw (IllegalStateException. "Session issued-at must not be blank")))
+    (let [issued-at (-> issued-at-str time-format/parse)]
+      (when-let [max-session-lifetime (:max_session_lifetime user)]
+        (let [max-session-lifetime-secs (duration/parse-string-to-seconds max-session-lifetime)]
+          (validate-session-lifetime! max-session-lifetime-secs issued-at)))
+      (let [max-session-lifetime (-> (get-config) :session :max_lifetime)]
+        (let [max-session-lifetime-secs (duration/parse-string-to-seconds max-session-lifetime)]
+          (validate-session-lifetime! max-session-lifetime-secs issued-at)))
+      )))
 
 (defn authenticate-session-cookie [request handler]
   (if-let [services-cookie (-> request keywordize-keys :cookies :cider-ci_services-session :value)]
@@ -38,6 +57,7 @@
            (validate! (-> session-object :signature)
                       (get-session-secret)
                       (-> user :password_digest))
+           (validate-expiration! user session-object)
            (when-not (:account_enabled user)
              (throw (IllegalStateException. "Account disabled!")))
            (handler (assoc request :authenticated-user user)))
