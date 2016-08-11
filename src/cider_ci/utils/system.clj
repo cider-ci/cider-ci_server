fa20f58c1b093ea0a5491838cbb2bc5bb689d1ca
Thomas Schank <DrTom@schank.ch>
2016-08-11 19:17:37 +0200

    Extend system ns with new exec functions and deprecate old ones

--- src/cider_ci/utils/system.clj
+++ src/cider_ci/utils/system.clj
@@ -18,13 +18,12 @@
     [org.apache.commons.lang3 SystemUtils]
     ))
 
-(defn create-watchdog []
-  (ExecuteWatchdog. (ExecuteWatchdog/INFINITE_TIMEOUT) ))
+(defn create-watchdog
+  ([]
+   (create-watchdog (ExecuteWatchdog/INFINITE_TIMEOUT)))
+  ([timeout-ms]
+   (ExecuteWatchdog. timeout-ms )))
 
-
-[(time/plus (time/now) (time/seconds 0.0))
- (time/plus (time/now) (time/millis 500.0))
- ]
 
 (defn normalized-timeout-ms [opts]
   (let [timeout (or (:timeout opts)
@@ -34,12 +33,13 @@
                              duration/parse-string-to-seconds
                              (* 1000.0))
       (integer? timeout) (* timeout 1.0)
-      :else 1000.0)))
+      :else (* 10 1000.0))))
 
 (defn exec
   ([command]
    (exec command {}))
   ([command opts]
+   (logging/warn "exec is deprecated, use exec! or async-exec")
    (when (:watchdog opts)
      (logging/warn (str "`" (clojure.string/join " " command) "`  "
                         "The :watchdog option is deprecated, use :timeout instead."
@@ -59,9 +59,8 @@
          (throw (ex-info (str "Execution `"
                               (clojure.string/join " " command)
                               "` expired.") opts)))))))
-
-
 (defn exec-with-success-or-throw [& args]
+   (logging/warn "exec-with-success-or-throw is deprecated, use exec! or async-exec")
   (let [res (apply exec args)]
     (if (= 0 (:exit res))
       res
@@ -74,6 +73,66 @@
 
 (defn exec-with-success? [& args]
   (= 0 (:exit (apply exec args))))
+
+
+;### NEW #######################################################################
+
+(defn cancle-async-exec [wrapped-exec]
+  (when (not (realized? (:exec @wrapped-exec)))
+    (swap! wrapped-exec (fn [r]
+                          (assoc r :exception
+                                 (ex-info (str "Execution `"
+                                               (clojure.string/join " " (:command r))
+                                               "` has been canceled!") {}))))
+    (.destroyProcess (-> @wrapped-exec :opts :watchdog))))
+
+(defn async-exec
+  ([command]
+   (async-exec command {}))
+  ([command opts]
+   (let [timeout-ms (normalized-timeout-ms opts)
+         expires-at (time/plus (time/now) (time/millis timeout-ms))
+         options (conj opts {:watchdog (create-watchdog)})
+         wrapped-exec (atom {:command command
+                             :opts options
+                             :exec (commons-exec/sh command options)})]
+     (future (while (and (not (realized? (-> @wrapped-exec :exec)))
+                         (time/before? (time/now) expires-at))
+               (Thread/sleep 50))
+             (when (not (realized? (:exec @wrapped-exec)))
+               (swap! wrapped-exec (fn [r]
+                                     (assoc r :exception
+                                            (ex-info (str "Execution `"
+                                                          (clojure.string/join " " (:command r))
+                                                          "` timed out!") r))))
+               (.destroyProcess (-> @wrapped-exec :opts :watchdog))))
+     wrapped-exec)))
+
+(defn exec!
+  ([command]
+   (exec! command {}))
+  ([command opts]
+   (let [wrapped-exec (async-exec command opts)
+         realized-exec (-> wrapped-exec deref :exec deref)]
+     (logging/warn 'wrapped-exec wrapped-exec)
+     (logging/warn 'realized-exec realized-exec)
+     (when-let [ex (-> wrapped-exec deref :exception)] (throw ex))
+     (when-not (= 0 (:exit realized-exec))
+       (throw  (ex-info (str "Execution `"
+                             (clojure.string/join " " (-> @wrapped-exec :command))
+                             "` exited with non zero status!")
+                        @wrapped-exec)))
+     realized-exec)))
+
+;(exec! ["exit" "1"])
+
+;(exec! ["sleep" "10"] {:timeout "3 Seconds"})
+
+;(exec! ["echo" "Hello World!"])
+
+
+;(def ^:dynamic *P* (async-exec ["sleep" "50"] {:timeout "3 Seconds"}))
+
 
 
 ;### Debug #####################################################################
