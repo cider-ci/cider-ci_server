9e88877367a3c43725a4813d2e3246605cd925ac
Thomas Schank <DrTom@schank.ch>
2016-11-13 18:07:15 +0100

    Improve and fix coner cases with row-envents

--- src/cider_ci/utils/row_events.clj
+++ src/cider_ci/utils/row_events.clj
@@ -10,24 +10,45 @@
     [logbug.thrown :as thrown]
     ))
 
+(defn process-last-row [table-name state-atom row-handler]
+  (swap! state-atom
+         (fn [_]
+           (when-let [last-row (I>> identity-with-logging
+                                    [(str "SELECT * FROM " table-name
+                                          " ORDER BY created_at DESC, id LIMIT 1")]
+                                    (jdbc/query (rdbms/get-ds))
+                                    first)]
+             (snatch {} (row-handler last-row))
+             last-row))))
+
+
+
+; 1. we compare to the timestamp of the row with the given id instead of
+;   the saved created_at because the latter might not be exactly the same
+;   (loosing bits due to coercion)
+; 2. this row might not even exist anymore; e.g. because it has been removed
+;     by the sweeper or something else (truncate); in that case we query all
+;     events of the last 24 hours as a fallback
+
+(defn process-new-rows [table-name state-atom row-handler]
+  (swap! state-atom
+         (fn [last-processed-row]
+           (or (I>> identity-with-logging
+                    [(str "SELECT * FROM " table-name
+                          " WHERE created_at > "
+                          "  (SELECT COALESCE(max(created_at), (now() - interval '24 hours')) "
+                          "   FROM " table-name " WHERE id = ?)"
+                          " ORDER BY created_at ASC LIMIT 1000")
+                     (:id last-processed-row)]
+                    (jdbc/query (rdbms/get-ds))
+                    (map (fn [row] (snatch {} (row-handler row)) row))
+                    last)
+               last-processed-row))))
+
 (defn process
   [table-name state-atom row-handler]
-  (when-not @state-atom
-    (when-let [last-row (I>> identity-with-logging
-                             [(str "SELECT * FROM " table-name
-                                   " ORDER BY created_at DESC, id LIMIT 1")]
-                             (jdbc/query (rdbms/get-ds))
-                             first)]
-      (snatch {} (row-handler last-row))
-      (reset! state-atom last-row)))
-  (when-let [after-row @state-atom]
-    (if-let [lst (I>> identity-with-logging
-                      [(str "SELECT * FROM " table-name
-                            " WHERE created_at >= ? AND id != ?"
-                            " ORDER BY created_at ASC , id LIMIT 100")
-                       (:created_at after-row) (:id after-row)]
-                      (jdbc/query (rdbms/get-ds))
-                      (map (fn [row] (snatch {} (row-handler row)) row))
-                      last)]
-      (reset! state-atom lst)
-      (reset! state-atom after-row))))
+  (locking state-atom
+    (if-not @state-atom
+      (process-last-row table-name state-atom row-handler)
+      (process-new-rows table-name state-atom row-handler))))
+
