562ece7f038604ae158f70eec9dbd5ab5d35daa6
Thomas Schank <DrTom@schank.ch>
2017-01-31 18:46:25 +0100

    Resurrect webstack exception

--- src/cider_ci/utils/ring.clj
+++ src/cider_ci/utils/ring.clj
@@ -6,11 +6,27 @@
 (ns cider-ci.utils.ring
   (:require
     [clojure.walk :refer [keywordize-keys]]
+    [cider-ci.WebstackException]
 
     [logbug.catcher :as catcher]
     [clojure.tools.logging :as logging]
-    ))
+    )
+
+  (:import [cider_ci WebstackException])
+  )
 
 
 (defn wrap-keywordize-request [handler]
   (fn [request] (-> request keywordize-keys handler)))
+
+(defn web-ex [s mp]
+  (WebstackException. s mp))
+
+(defn wrap-webstack-exception [handler]
+  (fn [request]
+    (catcher/snatch
+      {:level :warn
+       :return-fn #(if (instance? WebstackException %)
+                     (ex-data %)
+                     (throw %))}
+      (handler request))))
