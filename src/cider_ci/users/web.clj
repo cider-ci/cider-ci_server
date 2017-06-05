7100feaa419a6ab0ba5f0b63f07bd612789f8bb3
Thomas Schank <DrTom@schank.ch>
2017-06-05 20:28:55 +0200

    Authorize API-Token routes

--- src/cider_ci/users/web.clj
+++ src/cider_ci/users/web.clj
@@ -3,6 +3,7 @@
   (:require [cider-ci.utils.core :refer [keyword str]])
   (:require
     [cider-ci.users.api-tokens.core :as api-tokens]
+    [cider-ci.auth.authorize :as authorize]
 
     [compojure.core :as cpj]
     [ring.util.response]
@@ -19,12 +20,27 @@
   {:status 404
    :body "Not found in /users !"})
 
+(defn user? [req]
+  (= :user (-> req :authenticated-entity :type)))
+
+(defn authentication-method-not-token? [req]
+  (not= :token (-> req :authenticated-entity :authentication-method)))
+
+(defn route-id-matches-signed-in-user-id? [req]
+  (= (-> req :authenticated-entity :id str)
+     (-> req :route-params :id str)))
+
+(defn authorize? [req]
+  (and
+    (user? req)
+    (authentication-method-not-token? req)
+    (route-id-matches-signed-in-user-id? req)))
 
 (def routes
   (cpj/routes
-    (cpj/ANY "/users/:id/api-tokens/*" [] api-tokens/routes)
-    (cpj/ANY "*" [] dead-end-handler)
-    ))
+    (cpj/ANY "/users/:id/api-tokens/*" []
+             (authorize/wrap-require! api-tokens/routes authorize?))
+    (cpj/ANY "*" [] dead-end-handler)))
 
-
-
+;### Debug ####################################################################
+(debug/debug-ns *ns*)
