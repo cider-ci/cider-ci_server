f46b26fe9d4b64536a2b2b3e1bc700c0b72ff5ad
Thomas Schank <DrTom@schank.ch>
2018-08-10 15:44:20 +0200

    Specing git push

--- spec/shared-contexts/demo-project.rb
+++ spec/shared-contexts/demo-project.rb
@@ -1,10 +1,58 @@
 require 'spec_helper'
+require 'open3'
+
+module DemoProject
+  class << self
+
+    def demo_project_system_path 
+      Pathname(
+        "#{Dir.pwd}/demo-project/"
+      ).cleanpath.to_s
+    end
+
+    def exec! cmd
+      Dir.chdir demo_project_system_path do
+        wrappec_cmd = <<-CMD.strip_heredoc
+          #!/bin/env/bash
+          set -euxo
+        #{cmd}
+        CMD
+        Open3.popen3(wrappec_cmd) do |stdin, stdout, stderr, wait_thr|
+          exit_status = wait_thr.value
+          unless exit_status.success?
+            abort stderr.read
+          else
+            stdout.read.strip
+          end
+        end
+      end
+    end
+  end
+end
 
 shared_context :demo_project do
+
   let :demo_project_system_path do
     Pathname(
       "#{Dir.pwd}/demo-project/"
     ).cleanpath.to_s
+  end
+
+  before do
+    visit '/'
+    click_on_first 'Projects'
+    click_on_first 'Add project'
+    fill_in 'name', with: 'Demo-Project'
+    click_on_first 'Add project'
+    wait_until do 
+      page.has_content? 'Project "Demo-Project"'
+    end
+    visit '/'
+    click_on_first 'Commits'
+    DemoProject::exec! <<-CMD.strip_heredoc
+      git push http://#{@api_token_secret}@localhost:#{port}/projects/demo-project.git HEAD:master
+    CMD
+    binding.pry
   end
 end
 
